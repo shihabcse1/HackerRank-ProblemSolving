b5c63bb5375c436a2579d8d71ba2c8312aa6c75d
C-1/2 <tomdshihab@gmail.com>
2020-06-05 15:44:14 +0600

    Java 8

--- Compare the Triplets/Solution.java
+++ Compare the Triplets/Solution.java
@@ -6,23 +6,23 @@ public class Solution {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         int aliceScore = 0, bobScore = 0;
-
         int[] alice = new int[3];
         int[] bob = new  int[3];
-        for (int i = 0; i < 3; i++) {
+        
+        for (int i = 0; i < 3; i++) { // Taking Alice's Score by user input
             alice[i] = sc.nextInt();
         }
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 3; i++) { // Taking Bob's Score by user input
             bob[i] = sc.nextInt();
         }
-        for (int i = 0; i < 3; i++) {
-            if(alice[i] > bob[i]){
+        for (int i = 0; i < 3; i++) { 
+            if(alice[i] > bob[i]){ // if Alice's Score is greater than Bob then storing it in AliceScore variable.
                 aliceScore++;
             }
             if(bob[i] > alice[i]){
                 bobScore++;
             }
         }
-        System.out.println(aliceScore+" "+bobScore);
+        System.out.println(aliceScore+" "+bobScore); // Printing both Scores.
     }
 }
